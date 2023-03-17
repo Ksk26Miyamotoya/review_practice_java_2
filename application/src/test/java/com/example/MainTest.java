6305ffd656efe73d9f0c987fa1fd93259f1fdcbc
Kousuke Yamazaki <kousuke_yamazaki@pj100.biz>
2023-03-17 11:07:28 +0900

    ✨レビュー用コードコミット

--- application/src/test/java/com/example/MainTest.java
+++ application/src/test/java/com/example/MainTest.java
@@ -6,12 +6,30 @@ import org.junit.jupiter.api.Test;
 
 /**
  * サンプルテスト
- *
+ * @author A
  */
 public class MainTest {
     @Test
-    public void helloTest() {
-        var main = new Main();
-        assertEquals(main.hello(), "hello");
+    public void testFindMinimumValueWithEmptyArray() {
+        int[] array = new int[0];
+        int expected = Integer.MAX_VALUE;
+        int actual = Main.findMinimumValue(array);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindMinimumValueWithSingleValue() {
+        int[] array = {5};
+        int expected = 5;
+        int actual = Main.findMinimumValue(array);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindMinimumValueWithMultipleValues() {
+        int[] array = {5, 2, 9, 1, 6};
+        int expected = 1;
+        int actual = Main.findMinimumValue(array);
+        assertEquals(expected, actual);
     }
 }
